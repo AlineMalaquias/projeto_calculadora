ae3cbf1c48718bdf1b8b3da9226d707c812eab29
AlineMalaquias <alinemlqs21@gmail.com>
2024-12-20 12:50:57 -0300

    finalizando 2

---
+++ resto_divisao.py
@@ -0,0 +1,8 @@
+def divisao():
+    num1 = int(input("Insira o primeiro número: "))
+    num2 = int(input("Insira o segundo número: "))
+
+    resultado = num1 / num2
+
+    print(f"O resultado da divisao é: {resultado}")
+divisao()
