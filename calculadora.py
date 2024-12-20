b268b773dd3a2518c90f765523ec60a31669a522
AlineMalaquias <alinemlqs21@gmail.com>
2024-12-20 12:07:03 -0300

    acertando

--- calculadora.py
+++ calculadora.py
@@ -7,39 +7,42 @@ def calculadora():
         print("2 = Subtração")
         print("3 = Multiplicação")
         print("4 = Divisão")
-        print("5 = Sair")
+        print("5 = Memória")
+        print("6 = Sair")
 
         operacao = int(input("Insira o número da operação: "))
-        num1 = float(input("Insira o primeiro número: "))
-        num2 = float(input("Insira o segundo número: "))
-
-        if operacao == 5:
+        
+        if operacao == 6:
             print("Saindo da calculadora")
             break
 
-        if operacao == 1:
-            resultado = num1 + num2
-            memoria[0] = resultado
-            print(f"O resultado da soma é: {resultado}")
-        elif operacao == 2:
-            resultado = num1 - num2
-            memoria[0] = resultado
-            print(f"O resultado da subtração é: {resultado}")
-        elif operacao == 3:
-            resultado = num1 * num2
-            memoria[0] = resultado
-            print(f"O resultado da multiplicação é: {resultado}")
-        elif operacao == 4:
-            if num2 != 0:
-                resultado = num1 / num2
-                memoria[0] = resultado
-                print(f"O resultado da divisão é: {resultado}")
-            else:
-                print("Erro: Não é possível dividir por zero.")
-                print("")
-        
-        print(memoria.append(operacao))  
-        print(memoria[0])
-        continue
+        num1 = int(input("Insira o primeiro número: "))
+        num2 = int(input("Insira o segundo número: "))
 
+        if operacao == 5:
+          print (memoria)
+
+        elif operacao == 1:
+                resultado = num1 + num2
+                print(f"O resultado da soma é: {resultado}")
+                memoria.append (resultado)
+
+        elif operacao == 2:
+                resultado = num1 - num2
+                print(f"O resultado da subtração é: {resultado}")
+                memoria.append(resultado)
+        elif operacao == 3:
+                resultado = num1 * num2
+                print(f"O resultado da multiplicação é: {resultado}")
+                memoria.append(resultado)
+        elif operacao == 4:
+                if num2 != 0:
+                    resultado = num1 / num2
+                    print(f"O resultado da divisão é: {resultado}")
+                    memoria.append(resultado)
+                else:
+                    print("Erro: Não é possível dividir por zero.")
+                    print("")
+                    continue
+    
 calculadora()
