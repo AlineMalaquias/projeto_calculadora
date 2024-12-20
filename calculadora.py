a566a2539556729aed5dc7e99630600719788ef9
AlineMalaquias <alinemlqs21@gmail.com>
2024-12-20 12:29:45 -0300

    finalizando

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,5 @@
 def calculadora():
-    memoria = [0]
+    memoria = []
 
     while True:
         print("Escolha uma operação:")
@@ -11,38 +11,44 @@ def calculadora():
         print("6 = Sair")
 
         operacao = int(input("Insira o número da operação: "))
-        
+
         if operacao == 6:
             print("Saindo da calculadora")
             break
+        elif operacao == 5:
+            print("Exibir memória das operações:")
+            for operacao in memoria:
+                print(operacao)
+        else:
+            num1 = float(input("Digite o primeiro número: "))
+            num2 = float(input("Digite o segundo número: "))
 
-        num1 = int(input("Insira o primeiro número: "))
-        num2 = int(input("Insira o segundo número: "))
-
-        if operacao == 5:
-          print (memoria)
-
-        elif operacao == 1:
+            if operacao == 1:
                 resultado = num1 + num2
                 print(f"O resultado da soma é: {resultado}")
-                memoria.append (resultado)
-
-        elif operacao == 2:
+                memoria.append(resultado)
+            elif operacao == 2:
                 resultado = num1 - num2
                 print(f"O resultado da subtração é: {resultado}")
                 memoria.append(resultado)
-        elif operacao == 3:
+            elif operacao == 3:
                 resultado = num1 * num2
                 print(f"O resultado da multiplicação é: {resultado}")
                 memoria.append(resultado)
-        elif operacao == 4:
+            elif operacao == 4:
                 if num2 != 0:
                     resultado = num1 / num2
                     print(f"O resultado da divisão é: {resultado}")
                     memoria.append(resultado)
                 else:
                     print("Erro: Não é possível dividir por zero.")
-                    print("")
-                    continue
-    
+            else:
+                print("Operação inválida.")
+                continue
+
+            print("")
+            print(memoria[0])         
+            continue
+        print("")
+
 calculadora()
